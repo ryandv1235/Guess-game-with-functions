ac37c03145bc70e174c5a13b2613393a3e2cdaba
Ryan <devogelryan@gmail.com>
2020-07-13 06:52:45 +0200

    Second commit

--- main.py
+++ main.py
@@ -2,31 +2,20 @@ import random
 
 userInput = None
 
-def getAndValidateNumber():
-	notValid = True
-	while notValid:
-		print("You have to guess a number between 0-5")
+def Game():
+	inGame = True
+	while inGame:
+		print("Guess the number between 0-5")
 		userInput = input()
 		if userInput.isdigit():
 			userInput = int(userInput)
-			if userInput > 0 and userInput < 5:
-				print("You picked the number " + str(userInput))
-				notValid = False
-				randomNumber()
+			print("Your number is: " + str(userInput))
+			numberToGuess = random.randint(1, 4)
+			print("The random number: " + str(numberToGuess))
+			if userInput == numberToGuess:
+				print("You won!")
+				inGame = False
+			else:
+				print("You lost! Try again.")
 
-
-numberToGuess = None
-
-def randomNumber():
-	numberToGuess = random.randint(1, 4)
-	print("The Random number: " + str(numberToGuess))
-	checkWin(numberToGuess)
-	
-	
-def checkWin(numberToGuess):
-	if userInput == numberToGuess:
-		print("You won !")
-	else:
-		print("You lose !")
-
-getAndValidateNumber()
+Game()
